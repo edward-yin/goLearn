7fc0b08e6b96c4d1b16abf7f569d78d7bd5fdc81
Edward.Yin <edward.yinxp@gmail.com>
2024-03-06 22:58:28 +0800

    1

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# cd $1
-# gcc -Wall -g  -fsanitize=address -o main main.c -lm -std=c99 -I../include
-# # gcc -Wall -g  -o main main.c -lm -std=c99 -I../include
-# mv main ../bin/$1
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DIR=$1
+MAINCODE="${DIR}/main.go"
+go run $MAINCODE
